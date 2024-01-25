0659721a2e5e1ea057873a20c6d1290629a9526f
pmg369 <pablomarting19@gmail.com>
2024-01-25 16:29:57 -0300

    update tours page

--- public/js/tours.js
+++ public/js/tours.js
@@ -1,11 +1,14 @@
 let userName = prompt("What is your name?").toUpperCase();
 const span = document.querySelector("#welcome");
+const i = document.querySelector("i");
 
 while (userName.length < 3) {
     userName = prompt("The name must have at least 3 letters, tell us what is really your name?").toUpperCase();
 };
 
+i.setAttribute("class", "fa-solid fa-ticket fa-rotate-90 fa-xl");
 span.textContent = (`Hello, ${userName}.`);
+span.classList.add('md:text-xl');
 
 let userAge = Number(prompt("How old are you?"));
 let buttons = document.querySelectorAll ("button");
@@ -13,7 +16,7 @@ let buttons = document.querySelectorAll ("button");
 if(userAge < 18) {
     swal.fire("Sorry!!!", "Being a minor, you cannot buy tickets.", "info");
     for (let i = 0; i < buttons.length; i++) {
-        buttons[i].setAttribute("disabled","disabled");
+        buttons[i].setAttribute("disabled");
         buttons[i].style.backgroundColor = "gray";
         buttons[i].style.cursor = "default";
     };
@@ -42,7 +45,7 @@ function disableSoldOutButtons () {
         for(let city in tickets){
             if(tickets[city] == 0){
                 let buttons = document.getElementById("buy " + city);
-                buttons.innerHTML = "Sould Out!!!";
+                buttons.textContent = "Sould Out!!!";
                 buttons.style.background = ("gray");
                 buttons.disabled = true;
             };
